12ebef62da88b9d618420f2904d1c34858b4e29c
roy9 <roymurimi527@gmail.com>
2025-07-29 20:03:56 +0300

    modify react

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -3,7 +3,14 @@ import { currentAgeForBirthYear } from "../utils";
 describe("currentAgeForBirthYear", () => {
   it("returns the age of a person based on the year of birth", () => {
     const birthYear = 1984;
+    const expectedAge = new Date().getFullYear() - birthYear;
     const ageOfPerson = currentAgeForBirthYear(birthYear);
-    expect(ageOfPerson).toBe(38);
+    expect(ageOfPerson).toBe(expectedAge);
+  });
+
+  it("returns the current year for someone born in year 0", () => {
+    const currentYear = new Date().getFullYear();
+    const age = currentAgeForBirthYear(0);
+    expect(age).toBe(currentYear);
   });
 });
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,7 @@
-// Your code here
+// src/utils.js
+
+function currentAgeForBirthYear(birthYear) {
+  return new Date().getFullYear() - birthYear;
+}
+
+export { currentAgeForBirthYear };
